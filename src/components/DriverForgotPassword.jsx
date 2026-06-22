f0563c233c106cdca5d0362564d02ff3eb799583
narpat171 <ns7976144@gmail.com>
2026-06-22 07:36:30 +0530

    Add files via upload

--- src/components/DriverForgotPassword.jsx
+++ src/components/DriverForgotPassword.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 const DriverForgotPassword = () => {
   // 👇 यहाँ अपना Render या Glitch वाला बैकएंड लिंक डालें
-  const BASE_URL = "https://yahan-apna-backend-link-dale.onrender.com/api/driver"; 
+  const BASE_URL = "https://quickambu-backend-1.onrender.com"; 
 
   const navigate = useNavigate();
   const [step, setStep] = useState(1);
